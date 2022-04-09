934e55c5ca68d3e122b8d4e6ec0c4c0ff0da8d45
guruputran <gpsmartcodes@gmail.com>
2022-04-09 09:49:58 +0800

    first commit

---
+++ gatsby-config1.js
@@ -0,0 +1,24 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Ibas Majid",
+    description: "I teach JavaScript, React, GraphQL and Gatsby",
+    author: "Ibaslogic",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
+}
